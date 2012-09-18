52fb9b6ffad2e1384b3cece8953dd60cfdd7375d
Simon Unge <SimonUnge@gmail.com>
2012-09-18 15:24:53 +0200

    first commit

---
+++ hello-project/.worksheet/src/test.scala
@@ -0,0 +1,7 @@
+object test {import scala.runtime.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(57); 
+  println("Welcome to the Scala worksheet");$skip(12); 
+ 	val x = 1;System.out.println("""x  : Int = """ + $show(x ));$skip(31); 
+  def increase(i: Int) = i + 1;System.out.println("""increase: (i: Int)Int""");$skip(22); 
+  val y = increase(x);System.out.println("""y  : Int = """ + $show(y ))}
+	
+}
---
+++ hello-project/bin/greeter/test.sc
@@ -0,0 +1,7 @@
+object test {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+ 	val x = 1                                 //> x  : Int = 1
+  def increase(i: Int) = i + 1                    //> increase: (i: Int)Int
+  val y = increase(x)                             //> y  : Int = 2
+	
+}
